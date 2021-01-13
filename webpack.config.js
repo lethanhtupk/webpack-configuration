f61bac659062648f65cf31d0bc7b07eb6e8080c4
tult_tt <tult_tt@rikkeisoft.com>
2021-01-13 17:56:03 +0700

    config babel-loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,19 @@ module.exports = {
   output: {
     filename: 'application.js',
     path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
